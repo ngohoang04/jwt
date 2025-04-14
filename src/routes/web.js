aa051471532a36cb9a45430f4d6ff5fc961567b7
Trong Hoang <hoangngotrong2004@gmail.com>
2025-04-14 20:28:50 +0700

    mvc

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,11 +1,11 @@
 import express from 'express';
-
+import homeController from '../controller/homeController';
 const router = express.Router();
 
 const initWebRoutes = (app) => {
-    router.get('/', (req, res) => {
-        return res.send('home.ejs');
-    });
+    router.get('/',
+        homeController.handleHelloWorld
+    );
 
     return app.use('/', router);
 }
